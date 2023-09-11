8d03bc90c144186b6903bdda6d61e0b139a6a45b
Nicole <nicole.linares@sansano.usm.cl>
2023-09-11 20:05:49 -0300

    Mi perfil arreglado

--- src/pages/perfil.jsx
+++ src/pages/perfil.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Logocomp from '../assets/HHMlogo.png'
 import LogoUser from  '../assets/saul.png';
 import { Button} from 'react-bootstrap';
-import "../components/horario/style.css"
-import NavBar from '../components/nav_bar'
+import "../components/horario/style.css";
+import NavBar from '../components/nav_bar';
 
 export const Perfil = () => {
 
   const user = {
     nombre: 'Nombre del Usuario',
     correo: 'usuario@example.com',
-    contraseña: '********',
+    contrasena: '********',
     telefono: '1234567890',
     direccion: 'Calle 123, Colonia, Ciudad, Estado',
     carrera: 'Ingeniería en Computación',
@@ -29,23 +30,25 @@ export const Perfil = () => {
 
       <div className="container">
         <div className="row">
-          <div className="col-6">
+          <div className="col-1"></div>
+          <div className="col-4">
             <img src={LogoUser} className="img-fluid" />
           </div>
+          <div className="col-1"></div>
           <div className="col-6">
-            <div className="card-body text-end">
+            <div className="card-body">
               <p><strong>Nombre:</strong> {user.nombre}</p>
               <p><strong>Correo Electrónico:</strong> {user.correo}</p>
-              <p><strong>Contraseña:</strong> {user.contraseña}</p>
+              <p><strong>Contraseña:</strong> {user.contrasena}</p>
               <p><strong>Teléfono:</strong> {user.telefono}</p>
               <p><strong>Dirección:</strong> {user.direccion}</p>
               <p><strong>Carrera:</strong> {user.carrera}</p>
               <p><strong>Semestre:</strong> {user.semestre}</p>
               <p><strong>Rol:</strong> {user.rol}</p>
             </div>
+            <Button variant="primary">Editar</Button>
           </div>
         </div>
-        <Button variant="primary">Editar</Button>
       </div>
     </div>
   );
