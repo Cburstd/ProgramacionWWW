81594e52b2bf75f5f4cc165d94dd054b60e32097
FabianMunozI <fabian.munozi@usm.cl>
2023-12-03 23:15:29 -0300

    try5

--- src/components/formProfe.jsx
+++ src/components/formProfe.jsx
@@ -2,49 +2,6 @@ import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-
-
-function obtenerPerfiles() {
-  let query = `
-    query getUsuariosquery{
-        getUsuarios{
-            id
-            nombre_usuario
-            n_solicitudes_pendientes
-            n_prestamos_actuales
-            n_devoluciones_hechas
-            n_solicitudes_validadas
-        }
-    }
-  `;
-  $.ajax({
-      type: "POST",
-      url: "http://localhost:8090/graphql",
-      contentType: "application/json",
-      timeout: 15000,
-      data: JSON.stringify({
-        query: query,
-        variables: {}
-      }),
-      success: function(response) {
-        console.log(response);
-        let select = document.getElementById('usuario');
-        response.data.getPerfiles.forEach((element)=>{
-          const opt = document.createElement('option');
-          opt.value = element.id;
-          opt.text = element.nombre_usuario;
-          select.appendChild(opt);
-          console.log(element);
-        });
-      }
-  });
-
-  
-}
-
-  
-
 function BasicExampleProfe() {
   return (
     <form action="/action_page.php">
--- src/pages/reportes.jsx
+++ src/pages/reportes.jsx
@@ -6,12 +6,53 @@ import pronto from '../assets/soon.png'
 import BasicExampleProfe from '../components/formProfe'
 
 export const ReportesPage = () => {
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
+  function obtenerPerfiles() {
+  let query = `
+    query getUsuariosquery{
+        getUsuarios{
+            id
+            nombre_usuario
+            n_solicitudes_pendientes
+            n_prestamos_actuales
+            n_devoluciones_hechas
+            n_solicitudes_validadas
+        }
+    }
+  `;
+  $.ajax({
+      type: "POST",
+      url: "http://localhost:8090/graphql",
+      contentType: "application/json",
+      timeout: 15000,
+      data: JSON.stringify({
+        query: query,
+        variables: {}
+      }),
+      success: function(response) {
+        console.log(response);
+        let select = document.getElementById('usuario');
+        response.data.getPerfiles.forEach((element)=>{
+          const opt = document.createElement('option');
+          opt.value = element.id;
+          opt.text = element.nombre_usuario;
+          select.appendChild(opt);
+          console.log(element);
+        });
+      }
+  });
+  }
+  
+  
   return (
     <div className='page'>
       <h1 className='page__title'>Hamlin Hamlin & McGill</h1>
       <NavBar />
       <BasicExampleProfe/>
-    
+      <script>
+        obtenerPerfiles();
+      </script>
     </div>
 
   )
